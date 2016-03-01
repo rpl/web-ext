c3f7c652926f2ef7701209dca44d54780afee9f2
Luca Greco <luca.greco@alcacoop.it>
2016-03-01 20:06:03 +0100

    Add support for code coverage reports

--- tests/runner.js
+++ tests/runner.js
@@ -3,5 +3,23 @@
 
 import 'babel-polyfill';
 
+if (process.env.COVERAGE === 'y') {
+  // generate the coverage reports
+  after((done) => {
+    let istanbul = require('babel-istanbul');
+    let collector = new istanbul.Collector();
+    let reporter = new istanbul.Reporter();
+    let sync = false;
+
+    collector.add(global.__coverage__);
+
+    reporter.addAll([ 'text', 'text-summary', 'lcov' ]);
+    reporter.write(collector, sync, function() {
+      console.log('All reports generated');
+      done();
+    });
+  });
+}
+
 var context = require.context('.', true, /.*?test\..*?.js$/);
 context.keys().forEach(context);
